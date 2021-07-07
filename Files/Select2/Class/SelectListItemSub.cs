7766d33e66bf7f2826462f3bcabc43f77a44832d
pierre-gaborieau <55131762+pierre-gaborieau@users.noreply.github.com>
2021-07-07 11:10:24 +0200

    Add files via upload

---
+++ Files/Select2/Class/SelectListItemSub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Defontaine.Common
+{
+    class SelectListItemSub : SelectListItem
+    {
+        public string Sub { get; set; }
+    }
+}
