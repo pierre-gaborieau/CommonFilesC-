85568e1e3281b7974f7362a5c088def938f2197f
pierre-gaborieau <55131762+pierre-gaborieau@users.noreply.github.com>
2021-07-07 11:10:54 +0200

    Update SelectListItemSub.cs

--- Files/Select2/Class/SelectListItemSub.cs
+++ Files/Select2/Class/SelectListItemSub.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 
-namespace Defontaine.Common
+namespace pierregaborieau.Common
 {
     class SelectListItemSub : SelectListItem
     {
