ecacf9b1626e32bc8021bdc33c2cb6f5dd50f0a1
pierre-gaborieau <55131762+pierre-gaborieau@users.noreply.github.com>
2021-07-07 11:15:00 +0200

    Add files via upload

---
+++ Files/Select2/Class/Select2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Defontaine.Common
+{
+        public class Select2
+        {
+            public string id { get; set; }
+            public string text { get; set; }
+            public string sub { get; set; }
+        }
+
+}
---
+++ Files/Select2/Class/SelectList.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Defontaine.Common
+{
+    public class SelectList<T, U>
+    {
+        public IEnumerable<SelectListItem> GetSelectList(IEnumerable<U> items)
+        {
+            var selectList = new List<SelectListItem>();
+
+            foreach (var item in items)
+            {
+                selectList.Add(new SelectListItemSub
+                {
+                    Value = item.ToString(),
+                    Text = item.ToString(),
+                    Sub = item.ToString()
+                }) ;
+            }
+
+            return selectList;
+        }
+
+        public IEnumerable<Select2> ListSelect2(IEnumerable<U> items, string text, string id, string search, string sub)
+        {
+            List<Select2> tamponsSelect2 = new List<Select2>();
+
+            foreach (U item in items)
+            {
+                tamponsSelect2.Add(InitializerObject(text, id, item, sub));
+            }
+
+            if (!(string.IsNullOrEmpty(search) || string.IsNullOrWhiteSpace(search)))
+            {
+                tamponsSelect2 = tamponsSelect2.Where(x => x.text.ToLower().Contains(search.ToLower())).ToList();
+            }
+
+            return tamponsSelect2;
+        }
+
+        private Select2 InitializerObject(string text, string id, object item, string sub)
+        {
+            var valueText = item.GetType().GetProperty(text).GetValue(item, null).ToString();
+            var valueId = item.GetType().GetProperty(id).GetValue(item, null).ToString();
+            if(sub != null)
+            {
+                var valueSub = item.GetType().GetProperty(sub).GetValue(item, null).ToString();
+                return new Select2
+                {
+                    text = valueText,
+                    id = valueId,
+                    sub = valueSub
+                };
+            }
+
+            return new Select2
+            {
+                text = valueText,
+                id = valueId
+                
+            };
+        }
+    }
+}
